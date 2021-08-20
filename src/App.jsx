6d98d1f7155ed44970d7e8f4b90deeb2a8f33e3a
coffmanjrp <coffmanjrp@gmail.com>
2021-08-20 14:44:26 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,30 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="quiz-container">
+        <div className="quiz-header">
+          <h2>Question text</h2>
+          <ul>
+            <li>
+              <input type="radio" name="answer" id="a" className="answer" />
+              <label htmlFor="a"> Question</label>
+            </li>
+            <li>
+              <input type="radio" name="answer" id="b" className="answer" />
+              <label htmlFor="b"> Question</label>
+            </li>
+            <li>
+              <input type="radio" name="answer" id="c" className="answer" />
+              <label htmlFor="c"> Question</label>
+            </li>
+            <li>
+              <input type="radio" name="answer" id="d" className="answer" />
+              <label htmlFor="d"> Question</label>
+            </li>
+          </ul>
+        </div>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
